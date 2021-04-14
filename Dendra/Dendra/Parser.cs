6650db8bf752cf4579d25a564fdf29c2cfe14052
Dmitry <dima10des@gmail.com>
2021-04-15 02:03:03 +0300

    20

--- Dendra/Dendra/Parser.cs
+++ Dendra/Dendra/Parser.cs
@@ -16,14 +16,14 @@ namespace Dendra
 
         public static int Command (string input)
         {
-            if (input[0] == 'o') return 0; //Open
-            if (input[0] == 'c') return 1;  //Copy
-            if (input[0] == 'p') return 2;  //Past
-            if (input[0] == 'm') return 3;  //Move
-            if (input[0] == 'd') return 4;  //Delete
-            if (input[0] == 'r') return 5;  //Rename
-            if (input[0] == 'i') return 6;  //Info
-            if (input[0] == 'n') return 7;  //New folder
+            if (input[0] == 'o' && input[1] == ' ') return 0;  //Open
+            if (input[0] == 'c' && input[1] == ' ') return 1;  //Copy
+            if (input[0] == 'p' && input[1] == ' ') return 2;  //Past
+            if (input[0] == 'm' && input[1] == ' ') return 3;  //Move
+            if (input[0] == 'd' && input[1] == ' ') return 4;  //Delete
+            if (input[0] == 'r' && input[1] == ' ') return 5;  //Rename
+            if (input[0] == 'i' && input[1] == ' ') return 6;  //Info
+            if (input[0] == 'n' && input[1] == ' ') return 7;  //New folder
             else return -1;
         }
     }
--- Dendra/Dendra/Program.cs
+++ Dendra/Dendra/Program.cs
@@ -13,51 +13,22 @@ namespace Dendra
         static void Main(string[] args)
         {
             DendraInterface A = new DendraInterface();
-
             FileCrowler B = new FileCrowler(@"C:/");
             
             DendraInterface.Frame_actualizer(A,B);
             DendraInterface.Framing(A);
-            //DendraInterface.Cursor(0, A);
             
-            //DendraInterface.Framing(A);
-            //DendraInterface.Cursor(B.Cur_Subdirectories[0]);
             while(true)
             {
                 string input = Console.ReadLine();
-                Parser.Command(input);
+                if (Parser.Command(input) != -1)
+                {
+
+                }
                 DendraInterface.Frame_actualizer(A, B);
                 DendraInterface.Framing(A);
-            }
-
-            
+            }    
         }
-
-        ////        //static string[] Method1(string[] input)
-        ////        //{
-        ////        //    string[] output = new string[input.Length]; 
-
-        ////        //    for (var i=0;i<input.Length;i++)
-        ////        //    {
-
-        ////        //        output = Directory.GetFileSystemEntries(input[i]);
-        ////        //        return output;
-        ////        //    }
-        ////        //    Method1(output);
-        ////        //    return output;
-        ////        //}
-
-        ////        //Console.WriteLine("Введите путь каталога");
-        ////        //    string input = Console.ReadLine();
-        ////        //string[] entries = Directory.GetFileSystemEntries(input);
-        ////        //    //File.WriteAllLines("TreeByCycle.txt", entries);
-
-        ////        //    //, "*", SearchOption.AllDirectories
-        ////        //    for (int i = 0; i<entries.Length; i++)
-        ////        //    {
-        ////        //        Console.WriteLine(entries[i]);
-
     }
-
 }
 
